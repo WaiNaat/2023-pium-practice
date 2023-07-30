5096a66bf0c1a9a392282157cd17da3ca9fd86f5
Q Kim <qoli0703@gmail.com>
2023-07-30 11:33:36 +0900

    test: 간단한 메인페이지 테스트

---
+++ frontend/cypress/e2e/homepage.cy.ts
@@ -0,0 +1,17 @@
+describe('피움 메인 페이지', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:8282');
+  });
+
+  it('피움 소개 문구가 보인다.', () => {
+    cy.contains('식물을 쉽게');
+    cy.get('img[alt="logo"]');
+    cy.contains('피움에 등록된 식물을 검색해 보세요');
+  });
+
+  it('검색창을 이용해 식물을 검색할 수 있다.', () => {
+    cy.get('input').type('아카시').wait(300);
+    cy.contains('아카시');
+    cy.contains('아카시아');
+  });
+});
